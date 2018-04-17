dd38a24fdbc667860eaefeaf17821ae59e457be3
Stephen Goodall <stephen.goodall@SWL00159.sci-ware.local>
2018-04-17 11:29:04 +1000

    Multiple handlers attempt

--- src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
+++ src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
@@ -24,4 +24,14 @@ public class HelloWorldHandler implements RequestHandler<Object, Object> {
         headers.put("Content-Type", "application/json");
         return new GatewayResponse(new JSONObject().put("Output", "The Current time is: " + timeStampMillis).toString(), headers, 200);
     }
+    
+    public Object handleRequestES(final Object input, final Context context) {
+        
+        Instant instant = Instant.now();
+        long timeStampMillis = instant.toEpochMilli();
+
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        return new GatewayResponse(new JSONObject().put("Output", "La hora actual es: " + timeStampMillis).toString(), headers, 200);
+    }
 }
